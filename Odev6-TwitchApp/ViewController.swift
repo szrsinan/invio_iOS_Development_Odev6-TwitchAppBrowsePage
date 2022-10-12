b86435f4bedb71403201c270c9043beae89cb91b
SinanSezer <sinaan.sezer1@gmail.com>
2022-10-12 12:53:50 +0300

    Initial Commit

---
+++ Odev6-TwitchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev6-TwitchApp
+//
+//  Created by Sinan Sezer on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
